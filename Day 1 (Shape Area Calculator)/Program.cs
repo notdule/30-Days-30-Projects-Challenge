c7724c28e9068390a97cdc5779c648ec5bd9b32d
notdule <148584497+notdule@users.noreply.github.com>
2024-03-21 15:15:13 +0100

    Logic of the App

--- Day 1 (Shape Area Calculator)/Program.cs
+++ Day 1 (Shape Area Calculator)/Program.cs
@@ -1,1 +1,38 @@
+using System;
 
+namespace ShapeArea;
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string answer;
+        float result;
+
+        Console.WriteLine("What shape would you like to find the area of? Please enter 'r' for rectangle or any other key for circle.");
+
+        answer = Console.ReadLine();
+
+        if(answer == "r")
+        {
+            Console.WriteLine("Please enter the height of the rectangle.");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the width of the rectangle.");
+            float width = float.Parse(Console.ReadLine());
+
+            result = height * width;
+        }
+        else
+        {
+            Console.WriteLine("Please enter the radius of the circle");
+            float radius = float.Parse(Console.ReadLine());
+
+            result = (float)Math.PI * (radius * radius);
+        }
+
+        Console.WriteLine("The result is " + result);
+        Console.ReadKey();
+    }
+}
