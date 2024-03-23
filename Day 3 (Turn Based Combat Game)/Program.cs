f856ae8513e49875b063958694ea6962011825de
notdule <148584497+notdule@users.noreply.github.com>
2024-03-23 23:41:29 +0100

    Code & Logic!

--- Day 3 (Turn Based Combat Game)/Program.cs
+++ Day 3 (Turn Based Combat Game)/Program.cs
@@ -1,1 +1,71 @@
+using System;
 
+namespace TurnBased
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHp = 40;
+            int enemyHp = 20;
+
+            int playerAttack = 5;
+            int enemyAttack = 7;
+
+            int healAmount = 5;
+
+            Random random = new Random();
+
+            while(playerHp > 0 && enemyHp > 0)
+            {
+                // Player Turn
+                Console.WriteLine("-- Player turn --");
+                Console.Writeline("Player Hp = " + playerHp + ". Enemy Hp = " + enemyHp);
+                Console.WriteLine("Enter 'a' to attack or 'h' to heal");
+
+                string choice = Console.ReadLine();
+
+                if (choice == "a")
+                {
+                    enemyHp -= playerAttack;
+                    Console.WriteLine("Player attack enemy and deals " + playerAttack + " damage!");
+                }
+                else
+                {
+                    playerHp += healAmount;
+                    Console.WriteLine("Player restores " + healAmount + " health points!");
+                }
+
+                // Enemy Turn
+                if (enemyHp > 0)
+                {
+                    Console.WriteLine("-- Enemy turn --");
+                    Console.Writeline("Player Hp = " + playerHp + ". Enemy Hp = " + enemyHp);
+                    int enemyChoice = random.Next(0, 2);
+
+                    if (enemyChoice == 0)
+                    {
+                        playerHp -= enemyAttack;
+                        Console.WriteLine("Enemy attacks and deals " + enemyAttack + " damage!");
+
+                    }
+                    else
+                    {
+                        enemyHp += healAmount;
+                        Console.WriteLine("Enemy restores " + healAmount + " health points!");
+                    }
+                }
+            }
+
+            if (playerHp > 0)
+            {
+                Console.WriteLine("Congratulations, you have won!");
+            }
+            else
+            {
+                Console.WriteLine("You Lose!");
+            }
+        }
+
+    }
+}
