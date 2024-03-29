9bd9286b51bc13a033e253e3ecd7421247723f10
notdule <148584497+notdule@users.noreply.github.com>
2024-03-29 23:08:56 +0100

    Prototype 1

--- Day 9 (Music Player)/Program.cs
+++ Day 9 (Music Player)/Program.cs
@@ -1,1 +1,45 @@
+using System;
+using System.Windows.Forms;
+using System.Media;
 
+namespace MusicPlayerApp
+{
+    public partial class MusicPlayerForm : Form
+    {
+        private SoundPlayer player;
+
+        public MusicPlayerForm()
+        {
+            InitializeComponent();
+            player = new SoundPlayer();
+        }
+
+        private void btnPlayClick(object sender, EventArgs e)
+        {
+            player.Stream = Properties.Resources.SampleMusic;
+            player.Play();
+        }
+
+        private void btnPauseClick(object sender, EventArgs e)
+        {
+            if (player != null && player.Stream != null)
+            {
+                player.Stop();
+            }
+        }
+
+        private void btnStopClick(object sender, EventArgs e)
+        {
+            if (player != null && player.Stream != null)
+            {
+                player.Stop();
+                player.Stream.Position = 0;
+            }
+        }
+
+        private void trackBarVolumeScroll(object sender, EventArgs e)
+        {
+            player.Volume = trackBarVolume.Value;
+        }
+    }
+}
