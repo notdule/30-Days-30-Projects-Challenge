6c9d5dcc7f62e9b84735805330fd7f63cd256a13
notdule <148584497+notdule@users.noreply.github.com>
2024-04-02 23:20:37 +0200

    Code logic

--- Day 13 (Machine IP Address)/Program.cs
+++ Day 13 (Machine IP Address)/Program.cs
@@ -1,1 +1,24 @@
-
+using System;
+using System.Net;
+namespace Program
+{
+    class Program
+    {
+        static void Main()
+        {
+            // getting the Host Name.
+            String strHostName = string.Empty; 
+            strHostName = Dns.GetHostName();
+            Console.WriteLine("Local Machine's Host Name: " + strHostName);
+            IPHostEntry ipEntry = Dns.GetHostEntry(strHostName);
+            // Using Host Name,IP address is obtained.
+            IPAddress[] addr = ipEntry.AddressList;
+ 
+            for (int i = 0; i < addr.Length; i++)
+            {
+                Console.WriteLine("IP Address {1} : ",addr[i].ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
