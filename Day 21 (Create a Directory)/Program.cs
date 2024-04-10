694ac78874a2a743b70b8930bd66c1f41353bede
notdule <148584497+notdule@users.noreply.github.com>
2024-04-10 23:44:01 +0200

    Update Program.cs

--- Day 21 (Create a Directory)/Program.cs
+++ Day 21 (Create a Directory)/Program.cs
@@ -1,1 +1,12 @@
-
+using System;
+using System.IO;
+class program
+{
+    public static void Main()
+    {
+        Directory.CreateDirectory("C:\\NewDirectory");
+        Console.WriteLine("NewDirectory is Created in C Directory");
+        Console.ReadLine();
+ 
+    }
+}
